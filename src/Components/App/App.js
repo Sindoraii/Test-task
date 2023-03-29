d7ce46dea8c9c40c1d9153f5944e6ba5a9fdfbf8
Maria Krupenina <marise.sindorai@yandex.ru>
2023-03-29 19:27:18 +0400

    fix: get data via fake api instead of stub data

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {SortingUsers} from "../SortingUsers/SortingUsers";
 import {DataSource} from "../DataSource/DataSource";
-
 export function App() {
-    return (
-        <>
-            <SortingUsers users={DataSource()}/>
-        </>
-    );
+    const [users,setUsers] = useState([]);
+    const [isLoading,setIsLoading] = useState(false);
+    const [error,setError] = useState(null);
+
+    useEffect( () =>  {
+        (async function fetchData() {
+            setIsLoading(true);
+
+            const res = await DataSource();
+            if(Array.isArray(res)) {
+                setIsLoading(false)
+                setUsers(res)
+            } else {
+                setIsLoading(false);
+                setError(res.message);
+            }
+        })();
+    },[])
+
+    if (isLoading) {
+        return (
+            <h1>Loading...</h1>
+        )
+    } else if(error) {
+        return(
+            <h1>Error:{error}</h1>
+        )
+    } else {
+        return(
+            <SortingUsers users={users}/>
+        )
+    }
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from "./Components/App/App";
-// import {DataSource} from "./Components/DataSource/DataSource";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {UserProfile} from "./Components/UserProfile/UserProfile";
-
-// fetch('https://jsonplaceholder.typicode.com/users')
-//     .then(function(response){
-//         return response.json();
-//     }).then(function(data) {
-//     DataSource(data);
-// })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
