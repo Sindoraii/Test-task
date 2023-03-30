82670139a5822a8bcdbb1743bbcdc7c51357a373
Maria Krupenina <marise.sindorai@yandex.ru>
2023-03-30 10:04:24 +0400

    fix: fix problem with renderind before getting users from fake api

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,16 +2,16 @@ import React, {useEffect, useState} from "react";
 import {SortingUsers} from "../SortingUsers/SortingUsers";
 import {DataSource} from "../DataSource/DataSource";
 export function App() {
-    const [users,setUsers] = useState([]);
-    const [isLoading,setIsLoading] = useState(false);
-    const [error,setError] = useState(null);
-
-    useEffect( () =>  {
+    const [users, setUsers] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    console.log(users)
+    useEffect(() => {
         (async function fetchData() {
             setIsLoading(true);
 
             const res = await DataSource();
-            if(Array.isArray(res)) {
+            if (Array.isArray(res)) {
                 setIsLoading(false)
                 setUsers(res)
             } else {
@@ -19,19 +19,21 @@ export function App() {
                 setError(res.message);
             }
         })();
-    },[])
+    }, [])
 
-    if (isLoading) {
-        return (
-            <h1>Loading...</h1>
-        )
-    } else if(error) {
-        return(
-            <h1>Error:{error}</h1>
-        )
-    } else {
-        return(
-            <SortingUsers users={users}/>
-        )
+    if (users) {
+        if (isLoading) {
+            return (
+                <h1>Loading...</h1>
+            )
+        } else if (error) {
+            return (
+                <h1>Error:{error}</h1>
+            )
+        } else {
+            return (
+                <SortingUsers users={users}/>
+            )
+        }
     }
 }
--- src/Components/SortingUsers/SortingUsers.js
+++ src/Components/SortingUsers/SortingUsers.js
@@ -3,44 +3,20 @@ import {MainLayout} from "../Common/MainLayout/MainLayout";
 import {SortingUsersActionPanel} from "./SortingUsersActionPanel/SortingUsersActionPanel";
 import {SortingUsersList} from "./SortingUsersList/SortingUsersList";
 
-export function SortingUsers(props) {
-    // const users = [...props.users];
-
-    const [sortedUsers, setSortedUsers] = useState(props.users);
+export function SortingUsers({users}) {
+    const [sortedUsers, setSortedUsers] = useState(users);
     console.log('sortedUsers',sortedUsers);
-    console.log('setState',setSortedUsers )
 
 
     function getUsersSorted(field){
         let sortedUsersByField;
-
         if(field ==='city') {
-
             sortedUsersByField =  sortedUsers.sort((firstUser,secondUser)=> firstUser.address.city > secondUser.address.city ? 1 : -1);
-            console.log('CITY',sortedUsersByField)
         } else if(field === 'company') {
             sortedUsersByField = sortedUsers.sort((firstUser,secondUser)=>firstUser.company.name > secondUser.company.name ? 1 : -1);
         }
          setSortedUsers(sortedUsersByField);
     }
-
-       // let sortedUsersByField;
-
-    //    switch (field) {
-    //         case 'city':
-    //              let sortedUsersByFieldCity =  sortedUsers.sort((firstUser,secondUser)=> firstUser.address.city > secondUser.address.city ? 1 : -1);
-    //             setSortedUsers(sortedUsersByFieldCity);
-    //             console.log('HANDLER city',sortedUsers);
-    //             // console.log('sortedUsers',sortedUsers)
-    //             break;
-    //         case 'company':
-    //             let sortedUsersByFieldCompany =  sortedUsers.sort((firstUser,secondUser)=>firstUser.company.name - secondUser.company.name ? 1 : -1);
-    //             setSortedUsers(sortedUsersByFieldCompany);
-    //             break;
-    //     }
-    //     // console.log('HANDLER ',sortedUsersByField);
-    //     // setSortedUsers(sortedUsersByField);
-    // }
 
 
     return (
