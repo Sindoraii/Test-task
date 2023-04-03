54cd09075468cf3156ff563f39ac9dac34dae82c
Maria Krupenina <marise.sindorai@yandex.ru>
2023-04-03 16:08:47 +0400

    fix: change path for deployment

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<App/>} />
-            <Route path = "/profile" element = {<UserProfile/>} />
+            <Route path="/Test-task" element={<App/>} />
+            <Route path = "/Test-task/profile" element = {<UserProfile/>} />
             <Route path="*" element = {
                 <div>Страница не найдена</div>
             }/>
